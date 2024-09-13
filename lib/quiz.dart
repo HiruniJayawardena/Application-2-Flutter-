0244e8d7d604123f5fb73476ecafb7bebae3ba6b
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-09-14 05:13:48 +0530

    startScreen method added

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:starting_screen/questions_screen.dart';
 import 'package:starting_screen/start_screen.dart';
+import 'package:starting_screen/questions_screen.dart';
 
 class Quiz extends StatefulWidget{
   const Quiz({super.key});
@@ -14,6 +16,15 @@ class Quiz extends StatefulWidget{
 class _QuizState extends State<Quiz>{ //private class start with "_" and with class name and "State"
   // State is a generic class that takes the StatefulWidget as a type argument
   
+  Widget activeScreen = const StartScreen(); //activeScreen is a variable that holds the StartScreen widget
+  // we can't use var as the type because we need more general type. because we assign a new widget to it later
+
+  void switchScreen(){
+    setState((){
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context){
     return MaterialApp(
@@ -25,7 +36,7 @@ class _QuizState extends State<Quiz>{ //private class start with "_" and with cl
             Color.fromARGB(255, 204, 69, 245),
             Color.fromARGB(255, 136, 16, 86)
           ], begin: Alignment.topCenter, end: Alignment.bottomLeft)),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
