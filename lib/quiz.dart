3d06783d47b2cc838c0d1adb72383623a11a3c35
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-09-14 06:44:15 +0530

    another method to use start screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,40 +3,59 @@ import 'package:starting_screen/questions_screen.dart';
 import 'package:starting_screen/start_screen.dart';
 import 'package:starting_screen/questions_screen.dart';
 
-class Quiz extends StatefulWidget{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   // CreateState creates an instance of a class that based on state connected to quiz
   @override
-  State<Quiz> createState(){
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
-class _QuizState extends State<Quiz>{ //private class start with "_" and with class name and "State"
-  // State is a generic class that takes the StatefulWidget as a type argument
-  
- // Widget activeScreen = const StartScreen(switchScreen);
-  //activeScreen is a variable that holds the StartScreen widget
-  // (switchScreen) passess a pointer as a value to StartScreen
-  // we can't use var as the type because we need more general type. because we assign a new widget to it later
+class _QuizState extends State<Quiz> {
+  /* private class start with "_" and with class name and "State"
+   State is a generic class that takes the StatefulWidget as a type argument */
 
-  Widget? activeScreen; // we can use "?" to make it nullable (means can contain widget or null)
+  //-------------------------method -1----------------------------------------------
+  // Widget activeScreen = const StartScreen(switchScreen);
+  /* activeScreen is a variable that holds the StartScreen widget
+    (switchScreen) passess a pointer as a value to StartScreen
+     we can't use var as the type because we need more general type. because we assign a new widget to it later */
 
-  @override //for additional initialization
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState(); //call the parent class's initState method
-  }
+  // Widget?
+  //     activeScreen; // we can use "?" to make it nullable (means can contain widget or null)
 
-  void switchScreen(){
-    setState((){
-      activeScreen = const QuestionsScreen();
+  // @override //for additional initialization
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState(); //call the parent class's initState method
+  // }
+
+  // void switchScreen() {
+  //   setState(() {
+  //     activeScreen = const QuestionsScreen();
+  //   });
+  // }
+
+  //-------------------------method -1----------------------------------------------
+
+  //------another method to use activeScreen(method-2)
+  // This doesn't need init function
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
     });
   }
 
+  //--------------------------------------------------------------------------------
+
+  
+
   @override
-  Widget build(context){
+  Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -46,7 +65,14 @@ class _QuizState extends State<Quiz>{ //private class start with "_" and with cl
             Color.fromARGB(255, 204, 69, 245),
             Color.fromARGB(255, 136, 16, 86)
           ], begin: Alignment.topCenter, end: Alignment.bottomLeft)),
-          child: activeScreen,
+          //-------------------------method -1----------------------------------------------
+          // child: activeScreen,
+          //-------------------------method -1----------------------------------------------
+          
+          //------another method to use activeScreen(method-2)
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
