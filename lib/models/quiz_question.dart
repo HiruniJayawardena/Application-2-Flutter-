b163574d1b73e267c3652ddbdf6faa8a8a18fc14
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2024-09-15 08:10:26 +0530

    Questions and answers added

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,1 +1,7 @@
 // this does not contain a widget, but contain question blueprint 
+class QuizQuestion{
+  const QuizQuestion(this.text, this.answers);  // constructor function
+
+  final String text;
+  final List<String> answers;
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,32 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsScreenState extends State<QuestionsScreen>{
   @override
   Widget build(context){
-    return const Text('Questions screen!!!');
+    return SizedBox( // another approach as Center widget
+      width: double.infinity, // take the full width of the screen
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Text('The question....'),
+        const SizedBox(height: 20,),
+        ElevatedButton(
+          onPressed: (){}, 
+          child: const Text('Answer 1'),
+        ),
+         ElevatedButton(
+          onPressed: (){}, 
+          child: const Text('Answer 2'),
+        ),
+         ElevatedButton(
+          onPressed: (){}, 
+          child: const Text('Answer 3'),
+        ),
+         ElevatedButton(
+          onPressed: (){
+            print('Answer 4');
+          }, 
+          child: const Text('Answer 4'),
+        )
+      ],),
+    );
   }
 }
